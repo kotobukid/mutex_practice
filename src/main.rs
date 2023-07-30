dc7b4237528f1dec85fe211e5822b9a6e83bd6b6
kotobukid <kotobuki@daqua.jp>
2023-07-31 02:00:44 +0900

    このようにすべきらしい

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,21 @@ use std::sync::{Arc, Mutex, MutexGuard};
 use axum::response::{Html, IntoResponse};
 
 struct AppState {
-    count: i32,
+    count1: i32,
+    count2: i32,
 }
 
 #[tokio::main]
 async fn main() {
-    let shared_state: Arc<Mutex<AppState>> = Arc::new(Mutex::new(AppState { count: 0 }));
-    let shared_state_sub: Arc<Mutex<AppState>> = Arc::new(Mutex::new(AppState { count: 10000 }));
+    let shared_state: Arc<Mutex<AppState>> = Arc::new(Mutex::new(AppState { count1: 0, count2: 10000 }));
 
-    let router1 = Router::new().route("/", get(hello_world_handler)).with_state(shared_state);
-    let router2 = Router::new().route("/", get(decr_handler)).with_state(shared_state_sub);
+    let router1 = Router::new().route("/", get(hello_world_handler));
+    let router2 = Router::new().route("/", get(decr_handler));
 
     let app: Router = Router::new()
         .nest("/", router1)
         .nest("/decr", router2)
+        .with_state(shared_state)
         ;
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -33,15 +34,16 @@ async fn hello_world_handler(
     State(state): State<Arc<Mutex<AppState>>>
 ) -> impl IntoResponse {
     let mut state: MutexGuard<AppState> = state.lock().unwrap();
-    println!("{}", state.count);
-    state.count = state.count + 1;
-    Html(format!("<a href=\"/decr\">decr</a><br /><a href=\"/\">incr</a><br /><span>current: {}</span>", state.count))
+    println!("{}", state.count1);
+    state.count1 = state.count1 + 1;
+    Html(format!("<a href=\"/decr\">decr</a><br /><a href=\"/\">incr</a><br /><span>current: {}</span>", state.count1))
 }
+
 async fn decr_handler(
     State(state): State<Arc<Mutex<AppState>>>
 ) -> impl IntoResponse {
     let mut state: MutexGuard<AppState> = state.lock().unwrap();
-    println!("{}", state.count);
-    state.count = state.count - 1;
-    Html(format!("<a href=\"/decr\">decr</a><br /><a href=\"/\">incr</a><br /><span>current: {}</span>", state.count))
+    println!("{}", state.count2);
+    state.count2 = state.count2 - 1;
+    Html(format!("<a href=\"/decr\">decr</a><br /><a href=\"/\">incr</a><br /><span>current: {}</span>", state.count2))
 }
