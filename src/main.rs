bb8626d67692dc08e96ee0cb6ebab6aced311301
kotobukid <kotobuki@daqua.jp>
2023-07-31 01:35:55 +0900

    アクセスカウンター的な挙動をArc<Mutex>で宣言する

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 use axum::{
+    extract::{State},
     routing::get,
     Router,
 };
+use std::sync::{Arc, Mutex, MutexGuard};
+use axum::response::IntoResponse;
+
+struct AppState {
+    count: i32,
+}
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let shared_state: Arc<Mutex<AppState>> = Arc::new(Mutex::new(AppState { count: 0 }));
 
-    // run it with hyper on localhost:3000
+    let app: Router = Router::new()
+        .route("/", get(hello_world_handler))
+        .with_state(shared_state);
+
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn hello_world_handler(
+    State(state): State<Arc<Mutex<AppState>>>
+) -> impl IntoResponse {
+    let mut state: MutexGuard<AppState> = state.lock().unwrap();
+    println!("{}", state.count);
+    state.count = state.count + 1;
+    "Hello, World!"
 }
