41cfd9576571adc81c9ad5f11321a7f531deb137
kotobukid <kotobuki@daqua.jp>
2023-07-31 01:51:47 +0900

    State(with_state)はアプリケーション全体で状態を共有するためのもので、ルーター単位で持つこのような記述は本来NGらしい、が動作している。ChatGPT曰く新版Axumだからではないかとのこと。しかし設計や可読性の問題もあるためやはり推奨しないということであった。

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use axum::{
     Router,
 };
 use std::sync::{Arc, Mutex, MutexGuard};
-use axum::response::IntoResponse;
+use axum::response::{Html, IntoResponse};
 
 struct AppState {
     count: i32,
@@ -13,10 +13,14 @@ struct AppState {
 #[tokio::main]
 async fn main() {
     let shared_state: Arc<Mutex<AppState>> = Arc::new(Mutex::new(AppState { count: 0 }));
+    let shared_state_sub: Arc<Mutex<AppState>> = Arc::new(Mutex::new(AppState { count: 10000 }));
 
     let app: Router = Router::new()
         .route("/", get(hello_world_handler))
-        .with_state(shared_state);
+        .with_state(shared_state)
+        .route("/decr", get(decr_handler))
+        .with_state(shared_state_sub)
+        ;
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -30,5 +34,13 @@ async fn hello_world_handler(
     let mut state: MutexGuard<AppState> = state.lock().unwrap();
     println!("{}", state.count);
     state.count = state.count + 1;
-    "Hello, World!"
+    Html(format!("<a href=\"/decr\">decr</a><br /><a href=\"/\">incr</a><br /><span>current: {}</span>", state.count))
+}
+async fn decr_handler(
+    State(state): State<Arc<Mutex<AppState>>>
+) -> impl IntoResponse {
+    let mut state: MutexGuard<AppState> = state.lock().unwrap();
+    println!("{}", state.count);
+    state.count = state.count - 1;
+    Html(format!("<a href=\"/decr\">decr</a><br /><a href=\"/\">incr</a><br /><span>current: {}</span>", state.count))
 }
