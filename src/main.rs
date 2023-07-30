4799413375f6bee275fc8d2bf155270742f5f150
kotobukid <kotobuki@daqua.jp>
2023-07-31 02:38:39 +0900

    このように統合された一つのStateを持たねばならないらしい。またSharedStatesは内部niArc<Mutex>>を抱えているため、derive(Clone)では済まないとのこと

--- src/main.rs
+++ src/main.rs
@@ -15,23 +15,35 @@ struct AppState2 {
     message: String,
 }
 
+struct SharedStates {
+    state1: Arc<Mutex<AppState>>,
+    state2: Arc<Mutex<AppState2>>,
+}
+
+impl Clone for SharedStates {
+    fn clone(&self) -> Self {
+        Self {
+            state1: Arc::clone(&self.state1),
+            state2: Arc::clone(&self.state2),
+        }
+    }
+}
 #[tokio::main]
 async fn main() {
-    let shared_state: Arc<Mutex<AppState>> = Arc::new(Mutex::new(AppState { count1: 0, count2: 10000 }));
-    let shared_state2 = Arc::new(Mutex::new(AppState2 { message: "Hello, World!".to_string() }));
+    let shared_states = SharedStates {
+        state1: Arc::new(Mutex::new(AppState { count1: 0, count2: 10000 })),
+        state2: Arc::new(Mutex::new(AppState2 { message: "Hello!".to_string() })),
+    };
 
     let router1 = Router::new()
         .route("/", get(hello_world_handler))
-        .route("/decr", get(decr_handler))
-        .with_state(shared_state)
-        .with_state(shared_state2.clone())
-        ;
-    let router2 = Router::new().route("/", get(message_handler))
-        .with_state(shared_state2);
+        .route("/decr", get(decr_handler));
+    let router2 = Router::new().route("/", get(message_handler));
 
     let app: Router = Router::new()
         .nest("/", router1)
         .nest("/message", router2)
+        .with_state(shared_states)
         ;
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -41,35 +53,33 @@ async fn main() {
 }
 
 async fn hello_world_handler(
-    State(state): State<Arc<Mutex<AppState>>>,
-    State(state2): State<Arc<Mutex<AppState2>>>,
+    State(shared_states): State<SharedStates>,
 ) -> impl IntoResponse {
-    let mut state: MutexGuard<AppState> = state.lock().unwrap();
-    println!("{}", state.count1);
-    state.count1 = state.count1 + 1;
+    let mut state1: MutexGuard<AppState> = shared_states.state1.lock().unwrap();
+    println!("{}", state1.count1);
+    state1.count1 = state1.count1 + 1;
 
-    let mut state2: MutexGuard<AppState2> = state2.lock().unwrap();
+    let mut state2: MutexGuard<AppState2> = shared_states.state2.lock().unwrap();
     state2.message = "last access: incr".into();
-    Html(format!("<a href=\"/decr\">decr</a><br /><a href=\"/\">incr</a><br /><span>current: {}</span><br /><span>{}</span>", state.count1, state2.message))
+    Html(format!("<a href=\"/decr\">decr</a><br /><a href=\"/\">incr</a><br /><span>current: {}</span><br /><span>{}</span>", state1.count1, state2.message))
 }
 
 async fn decr_handler(
-    State(state): State<Arc<Mutex<AppState>>>,
-    State(state2): State<Arc<Mutex<AppState2>>>,
+    State(shared_states): State<SharedStates>,
 ) -> impl IntoResponse {
-    let mut state: MutexGuard<AppState> = state.lock().unwrap();
-    println!("{}", state.count2);
-    state.count2 = state.count2 - 1;
+    let mut state1: MutexGuard<AppState> = shared_states.state1.lock().unwrap();
+    println!("{}", state1.count2);
+    state1.count2 = state1.count2 - 1;
 
-    let mut state2: MutexGuard<AppState2> = state2.lock().unwrap();
+    let mut state2: MutexGuard<AppState2> = shared_states.state2.lock().unwrap();
     state2.message = "last access: decr".into();
 
-    Html(format!("<a href=\"/decr\">decr</a><br /><a href=\"/\">incr</a><br /><span>current: {}</span><br /><span>{}</span>", state.count2, state2.message))
+    Html(format!("<a href=\"/decr\">decr</a><br /><a href=\"/\">incr</a><br /><span>current: {}</span><br /><span>{}</span>", state1.count2, state2.message))
 }
 
 async fn message_handler(
-    State(state): State<Arc<Mutex<AppState2>>>,
+    State(shared_states): State<SharedStates>,
 ) -> impl IntoResponse {
-    let state: MutexGuard<AppState2> = state.lock().unwrap();
-    state.message.clone()
+    let mut state2 = shared_states.state2.lock().unwrap();
+    state2.message.clone()
 }
